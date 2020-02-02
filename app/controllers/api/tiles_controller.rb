68c0ae5b51f237198f5fc490d6fa11e910979f76
nururi <znLWwUD@gmail.com>
2020-02-02 16:14:16 +0900

    add Api::TilesContoller

---
+++ app/controllers/api/tiles_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Api::TilesController < ApplicationController
+  include TilesIdentifiable
+
+  def index
+    tile_category_id = params[:tile_category_id]
+    tile_category = tile_categories.key(tile_category_id&.to_i)
+    tiles =
+      case tile_category
+      when :萬子 then characters
+      when :筒子 then dots
+      when :索子 then bamboos
+      when :風牌 then wings
+      when :三元牌 then dragons
+      else characters.merge(dots).merge(bamboos).merge(wings).merge(dragons)
+      end
+    render json: tiles
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@
 
 Rails.application.routes.draw do
   root controller: :top_page, action: :show
+  namespace :api do
+    resources :tiles, only: [:index]
+  end
 end
