b652e05c1c30dd0d9f9f520b2f2497d7c082fb29
nururi <znLWwUD@gmail.com>
2020-02-02 15:03:59 +0900

    improve sample values

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -5,26 +5,41 @@ class TopPageController < ApplicationController
 
   private
     def set_values
-      @first_values = {
-        'HTML5': 1,
-        'CSS3': 2,
-        'JavaScript': 3,
-        'jQuery': 4,
-        'AngularJS': 5,
-        'ReactJS': 6,
-        'ReactNative': 7,
-        'Bootstrap': 8,
-        'Vue.js': 9,
-        'Python': 10,
+      @first_values = tile_categories
+      @second_values = {}
+    end
+    # tiles
+    def tile_categories
+      {
+        '萬子': 0,
+        '筒子': 1,
+        '索子': 2,
+        '風牌': 3,
+        '三元牌': 4,
       }
-      @second_values = {
-        '1s': 1,
-        '2p': 22,
-        '3m': 33,
-        '東': 41,
-        '白': 51,
-        '発': 52,
-        '中': 53,
+    end
+    def characters
+      1..9.map { | number | ["#{number}萬", (10 * 0) + number ] }.to_h.symbolize_keys
+    end
+    def dots
+      1..9.map { | number | ["#{number}筒", (10 * 1) + number ] }.to_h.symbolize_keys
+    end
+    def bamboos
+      1..9.map { | number | ["#{number}索", (10 * 2) + number ] }.to_h.symbolize_keys
+    end
+    def wings
+      {
+        '東': (10 * 3) + 1,
+        '南': (10 * 3) + 2,
+        '西': (10 * 3) + 3,
+        '北': (10 * 3) + 4,
+      }
+    end
+    def dragons
+      {
+        '白': (10 * 4) + 1,
+        '発': (10 * 4) + 2,
+        '中': (10 * 4) + 3,
       }
     end
 end
