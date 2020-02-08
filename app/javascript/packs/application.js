eeb6e5a3729312c185f089f1abe6d2f787a0ed7c
nururi <znLWwUD@gmail.com>
2020-02-09 02:19:37 +0900

    add scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require('@rails/activestorage').start()
 require('channels')
 
 import '../tiles.js'
+import '../stylesheets/demo.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
